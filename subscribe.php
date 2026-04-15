f588aa6a82f47f4db565e8db51d6dd166ddfd5a2
Fezile Mkhonza <fezilemk11@gmail.com>
2026-04-15 13:08:49 +0200

    Update subscribe.php

--- subscribe.php
+++ subscribe.php
@@ -28,6 +28,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="utf-8">
     <title>Subscription - AMMS</title>
     <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="icon" type="image/jpeg" href="assets/images/favicon.jpg">
+    <link rel="shortcut icon" href="assets/images/favicon.jpg">
     <link rel="stylesheet" href="css/style.css">
 </head>
 
